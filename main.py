0f2a4a7014e7d2946a66a5d8f256a65bca4c4041
wyzarka <iandaroz@live.com>
2026-06-05 16:30:29 -0500

    v0.5 Ch3 L5 completed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
-from player import *
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -12,6 +12,8 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0.0
+    updatable, drawable = pygame.sprite.Group(), pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -20,8 +22,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for unit in drawable:
+            unit.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
